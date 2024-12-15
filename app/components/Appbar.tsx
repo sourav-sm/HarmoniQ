f729457b223f2be2f0422f70e467cc61303ff067
Sourav <souravmohanta4428@gmail.com>
2024-12-15 23:38:54 +0530

    added landing page

--- app/components/Appbar.tsx
+++ app/components/Appbar.tsx
@@ -18,43 +18,29 @@
 "use client";
 
 import { signIn, signOut, useSession } from "next-auth/react";
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
 
 export function Appbar() {
-    const { data: session, status } = useSession();
+    // const { data: session, status } = useSession();
 
+
+    const session=useSession();
     // Loading state
-    if (status === "loading") {
-        return <div>Loading...</div>;
-    }
-
-    return (
-        <div className="flex justify-between items-center p-4 bg-gray-100">
-            <div className="text-xl font-bold">
-                Harmoniq
-            </div>
-            <div>
-                {session?.user ? (
-                    <button
-                        className="m-2 p-2 bg-blue-400 text-white rounded"
-                        onClick={async () => {
-                            await signOut();
-                        }}
-                        aria-label="Logout"
-                    >
-                        Logout
-                    </button>
-                ) : (
-                    <button
-                        className="m-2 p-2 bg-blue-400 text-white rounded"
-                        onClick={async () => {
-                            await signIn();
-                        }}
-                        aria-label="Signin"
-                    >
-                        Signin
-                    </button>
-                )}
-            </div>
+    // if (status === "loading") {
+    //     return <div>Loading...</div>;
+    // }
+    return ( 
+    <div className="flex justify-between px-20 p-5">
+        <div className="text-xl font-bold flex flex-col justify-center text-white">
+            Harmoniq
         </div>
+        <div>
+            {session.data?.user && <Button className=" bg-purple-600 hover:bg-purple-700 text-white " onClick={()=>signOut()}>Logout</Button>}
+            {!session.data?.user && <Button className="bg-purple-600 hover:bg-purple-700 text-white " onClick={()=>signIn()}>Signin</Button>}
+        </div>
+    </div>
+
     );
 }
